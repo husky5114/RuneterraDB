494bb22589a5b7906e16a5432a08416602e2a747
husky5114 <thien5114@gmail.com>
2019-11-06 20:03:53 -0800

    v1

---
+++ Script.js
@@ -0,0 +1,32 @@
+var regionNames = ["all", "demacia","freljord","ionia","noxus","piltoverzuan","shadowisles"];
+regionNames.forEach(displayRegion);
+
+var regionCounts = [
+    ["DE",56],
+    ["FR",57]    
+]
+displayCards(regionCounts);
+
+
+
+
+
+function displayRegion(name){
+    var image = document.createElement("img");
+    image.setAttribute("src", "Resources/Region_images/icon-" + name + ".png");
+    image.setAttribute("class", "regionImage");
+    document.getElementById("regions").appendChild(image);
+}
+
+function displayCards(rCounts){
+    for(var i=0; i < rCounts.length; ++i){
+        var region = rCounts[i][0];
+        for(var j=1; j <= rCounts[i][1]; ++j){
+            var image = document.createElement("img");
+            image.setAttribute("src", "Resources/Card_Images/01" + region + "0" + j.toString() + ".png");
+            image.setAttribute("class", "card");
+            document.getElementById("cards").appendChild(image);
+        }
+    }
+}
+
